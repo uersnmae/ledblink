86de2de2ca1348502989d5e8c471d726e88f9f0b
Dong Hyun Kim <dong-hki@c1r2s2.42gyeongsan.kr>
2025-07-27 17:00:59 +0900

    .

--- src/main.c
+++ src/main.c
@@ -8,8 +8,8 @@ int main(void) {
 	// 1 << 0 for GPIOA, 1 << 2 for GPIOC
 	RCC->AHB1ENR |= (1 << 0) | (1 << 2);
 
-	GPIOA->MODER &= ~(0b11 << 10); /* clear bits */
-	GPIOA->MODER |=  (0b01 << 10); /* set output mode */
+	// set output mode
+	GPIOA->MODER = (GPIOA->MODER & ~(0b11 << 10)) | (0b01 << 10);
 
 	GPIOC->MODER &= ~(0b11 << 26); /* clear bits, set input mode */
 

dea92d91c58a48241b25cd35925d24b24546b494
Dong Hyun Kim <dong-hki@c1r2s2.42gyeongsan.kr>
2025-07-27 15:27:39 +0900

    add USER BUTTON action
    now you can toggle LED02 with Blue User Button

--- src/main.c
+++ src/main.c
@@ -5,17 +5,20 @@ void delay(volatile uint32_t count) {
 }
 
 int main(void) {
-	RCC->AHB1ENR |= (1 << 0);
+	// 1 << 0 for GPIOA, 1 << 2 for GPIOC
+	RCC->AHB1ENR |= (1 << 0) | (1 << 2);
 
-	GPIOA->MODER &= ~(0b11 << 10);
-	GPIOA->MODER |=  (0b01 << 10);
+	GPIOA->MODER &= ~(0b11 << 10); /* clear bits */
+	GPIOA->MODER |=  (0b01 << 10); /* set output mode */
+
+	GPIOC->MODER &= ~(0b11 << 26); /* clear bits, set input modt */
 
 	while (1) {
-		GPIOA->BSRR = (1 << 5);
-		delay(500000);
-
-		GPIOA->BSRR = (1 << (5 + 16));
-		delay(500000);
+		if ((GPIOC->IDR & (1 << 13)) == 0) {
+			GPIOA->BSRR = (1 << 5);
+		} else {
+			GPIOA->BSRR = (1 << (5 + 16));
+		}
 	}
 	return 0;
 }
